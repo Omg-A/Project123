1ef3617008d7e76ef18f42b785c2f873b9e8d788
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-09-13 19:08:05 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+size = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,7 +16,11 @@ function setup(){
 }
 
 function draw(){
+    document.getElementById("font_size_of_text").innerHTML = "Size of the text will be = " + size;
+
     background('white');
+    textSize(size);
+    text('Hello', noseX, noseY);
 }
 
 function  modelLoaded(){
@@ -19,6 +29,11 @@ function  modelLoaded(){
 
 function gotPoses(results){
     if(results.length > 0){
-        console.log(results);
+        noseX = results[0].pose.nose.x - 200;
+        noseY = results[0].pose.nose.y;
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        size = floor(leftWristX - rightWristX);
     }
 }
